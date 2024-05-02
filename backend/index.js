c2b5d929488f430ba994a7ccf169fe6a97207f81
raazkhnl <raazkhnl@gmail.com>
2024-05-02 19:48:49 +0545

    v1.1

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-// const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
@@ -8,12 +7,10 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    // origin : process.env.FRONTEND_URL,
-    // credentials : true
+    origin : process.env.FRONTEND_URL,
+    credentials : true
 }))
 app.use(express.json())
-// app.use(cookieParser())
-
 app.use("/api",router)
 
 const PORT = 8080 || process.env.PORT
