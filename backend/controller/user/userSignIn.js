cb8325107eff587ded5dcb94a0bb689084e40bda
raazkhnl <raazkhnl@gmail.com>
2024-05-07 20:31:02 +0545

    vercel deployment cross-site

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -31,8 +31,7 @@ async function userSignInController(req,res){
         const token = await jwt.sign(tokenData, process.env.JWT_TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
 
         const tokenOption = {
-            httpOnly : true,
-            secure : true
+            httpOnly: true, secure: true, sameSite: 'None' 
         }
 
         res.cookie("token",token,tokenOption).status(200).json({
