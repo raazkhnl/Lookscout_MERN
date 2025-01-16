2c119e01b15d31ccc2f5611272b995dc57a48545
raazkhnl <raazkhnl@gmail.com>
2025-01-16 07:29:50 +0545

    route to profile

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 import Context from '../context';
 
 const LogIn = () => {
-  const { fetchUserDetails} = useContext(Context)
+  const { fetchUserDetails } = useContext(Context)
 
   const [data, setData] = useState({
     email: "",
@@ -28,52 +28,52 @@ const LogIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-      const dataResponse = await fetch(SummaryApi.logIn.url, {
-        method: SummaryApi.logIn.method,
-        credentials : 'include',
-        headers: {
-          "content-type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
+    const dataResponse = await fetch(SummaryApi.logIn.url, {
+      method: SummaryApi.logIn.method,
+      credentials: 'include',
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
 
-      const dataApi = await dataResponse.json()
+    const dataApi = await dataResponse.json()
 
-      if (dataApi.success) {
-        toast.success(dataApi.message)
-        navigate("/")
-        fetchUserDetails()
-      }
-
-      if (dataApi.error) {
-        toast.error(dataApi.message)
-      }
+    if (dataApi.success) {
+      toast.success(dataApi.message)
+      navigate("/profile")
+      fetchUserDetails()
     }
+
+    if (dataApi.error) {
+      toast.error(dataApi.message)
+    }
+  }
 
 
   return (
-<div className='container-fluid' style={{backgroundColor:'#151B28'}} id="contactUs">
-    <div className="row">
-      <div className="container col-10 col-md-6 my-5 text-white rounded p-4" style={{backgroundColor:'#1C2534'}}>
-        <div className='position-relative text-center'>
-          <img className="rounded-circle " style={{ height: '80px', width:'80px'}} src={loginIcons} alt='Login Icon'  />
-        </div>
+    <div className='container-fluid' style={{ backgroundColor: '#151B28' }} id="contactUs">
+      <div className="row">
+        <div className="container col-10 col-md-6 my-5 text-white rounded p-4" style={{ backgroundColor: '#1C2534' }}>
+          <div className='position-relative text-center'>
+            <img className="rounded-circle " style={{ height: '80px', width: '80px' }} src={loginIcons} alt='Login Icon' />
+          </div>
 
-        <form className='pt-6' onSubmit={handleSubmit}>
-          <label htmlFor='email' className='form-label'>Email :</label>
-          <input
-            id='email'
-            type='email'
-            placeholder='Enter email'
-            name='email'
-            value={data.email}
-            onChange={handleOnChange}
-            required
-            className='form-control mb-3'
-            autocomplete="username"
-          />
+          <form className='pt-6' onSubmit={handleSubmit}>
+            <label htmlFor='email' className='form-label'>Email :</label>
+            <input
+              id='email'
+              type='email'
+              placeholder='Enter email'
+              name='email'
+              value={data.email}
+              onChange={handleOnChange}
+              required
+              className='form-control mb-3'
+              autocomplete="username"
+            />
 
-          <label htmlFor='password' className='form-label'>Password :</label>
+            <label htmlFor='password' className='form-label'>Password :</label>
             <input
               id='password'
               type="password"
@@ -86,18 +86,18 @@ const LogIn = () => {
               autocomplete="current-password"
             />
 
-          <div className='text-center'>
-          <button
-            type='submit'
-            className='btn btn-primary mx-auto'
-          >
-            Login
-          </button>
-          </div>
-        </form>
+            <div className='text-center'>
+              <button
+                type='submit'
+                className='btn btn-primary mx-auto'
+              >
+                Login
+              </button>
+            </div>
+          </form>
 
-        <Link to={"/forgot-password"} className='text-danger my-3'>Forgot Password?</Link>
-      </div>
+          <Link to={"/forgot-password"} className='text-danger my-3'>Forgot Password?</Link>
+        </div>
       </div>
     </div>
 
